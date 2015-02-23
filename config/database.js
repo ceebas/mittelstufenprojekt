47c143ee539dcc678cd87d7b4ab3665fec8efe8c
Sebastian Schroeder <ses@i2dm.de>
2015-02-23 09:39:04 +0100

    Heroku db angebunden

--- config/database.js
+++ config/database.js
@@ -1,9 +1,8 @@
 // config/database.js
 module.exports = {
     'connection': {
-        'host': 'localhost',
-        'user': 'root',
-        'password': 'i2dm'
+        'host': 'postgres://cousjmswbqlidc:XXZ7bw1fi3CIv6AUNhggVtgZZ3@ec2-107-21-104-188.compute-1.amazonaws.com:5432/d4hombhs7kdqeo'
+        
     },
 	'database': 'browseagame_node',
     'tableUsers': "users",
